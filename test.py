7659598be8320af8eec4a29e1fcc08e6e8dd3fcd
Александр Макимович <cahr2001@mail.ru>
2021-04-06 13:40:58 +0300

    first

---
+++ test.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+def string(string, char):
+    i = 0
+    count = 0
+    while i < len(string):
+        if string[i] == char:
+            count += 1
+        i += 1
+    return count
+
+print(string('sasha', 's'))
+print(sqrt(16))
